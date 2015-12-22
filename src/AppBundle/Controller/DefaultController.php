5a19b71fa66acf8c03dfc77d1e8fcb6f60a383b9
Zanshee <info@zanshee.kiev.ua>
2015-12-22 10:35:11 +0200

    Registration added

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -3,9 +3,12 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\Message;
+use AppBundle\Entity\User;
 use AppBundle\Form\MessageType;
+use AppBundle\Form\UserType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\ClickableInterface;
 use Symfony\Component\HttpFoundation\Request;
 
 /**
@@ -28,27 +31,59 @@ class DefaultController extends Controller
     {
         $em = $this->getDoctrine()->getManager();
 
-        $list = $em->getRepository("AppBundle:Message")->findBy(array(), array("id" => "DESC"));
+        $list = $em->getRepository('AppBundle:Message')->findBy(array(), array('id' => 'DESC'));
 
         $message = new Message();
         $form = $this->createForm(new MessageType(), $message, array(
-            "action" => $this->generateUrl("guestbook"),
-            "method" => "POST",
+            'action' => $this->generateUrl('guestbook'),
+            'method' => 'POST',
         ));
-        $form->add("submit", "submit", array("label" => "Add message"));
+        $form->add('submit', 'submit', array('label' => 'Add message'));
         $form->handleRequest($request);
 
         if ($form->isValid()) {
             $em->persist($message);
             $em->flush();
 
-            return $this->redirectToRoute("guestbook");
+            return $this->redirectToRoute('guestbook');
         }
 
-        return $this->render("AppBundle::index.html.twig", array(
+        return $this->render('AppBundle::index.html.twig', array(
             'form' => $form->createView(),
             'list' => $list,
         ));
     }
 
+    /**
+     * User registration page
+     *
+     * @Route("/register", name="register")
+     *
+     * @param Request $request
+     *
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     */
+    public function registerAction(Request $request)
+    {
+        $user = new User();
+
+        $form = $this->createForm(new UserType(), $user, array(
+            'action' => $this->generateUrl('register'),
+            'method' => 'POST',
+        ));
+        $form->add('submit', 'submit', array('label' => 'Register'));
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            return $this->redirectToRoute('guestbook');
+        }
+
+        return $this->render('AppBundle::register.html.twig', array(
+            'form' => $form->createView(),
+        ));
+    }
 }
---
+++ src/AppBundle/Form/UserType.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+/**
+ * Class UserType
+ *
+ * @package AppBundle\Form
+ */
+class UserType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array                $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username')
+            ->add('email')
+        ;
+    }
+
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\User',
+        ));
+    }
+
+    /**
+     * Returns the name of this type.
+     *
+     * @return string The name of this type
+     */
+    public function getName()
+    {
+        return 'app_user';
+    }
+}
