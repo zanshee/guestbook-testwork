eadf6f1fb68d641eefd2c0cffa05c79291e4ebca
Zanshee <info@zanshee.kiev.ua>
2015-12-22 09:49:37 +0200

    Guestbook page added

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,20 +2,53 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Message;
+use AppBundle\Form\MessageType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
+/**
+ * Class DefaultController
+ *
+ * @package AppBundle\Controller
+ */
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * Guestbook. List of messages. New message form.
+     *
+     * @Route("/", name="guestbook")
+     *
+     * @param Request $request
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', array(
-            'base_dir' => realpath($this->container->getParameter('kernel.root_dir') . '/..'),
+        $em = $this->getDoctrine()->getManager();
+
+        $list = $em->getRepository("AppBundle:Message")->findBy(array(), array("id" => "DESC"));
+
+        $message = new Message();
+        $form = $this->createForm(new MessageType(), $message, array(
+            "action" => $this->generateUrl("guestbook"),
+            "method" => "POST",
+        ));
+        $form->add("submit", "submit", array("label" => "Add message"));
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em->persist($message);
+            $em->flush();
+
+            return $this->redirectToRoute("guestbook");
+        }
+
+        return $this->render("AppBundle::index.html.twig", array(
+            'form' => $form->createView(),
+            'list' => $list,
         ));
     }
+
 }
---
+++ src/AppBundle/Form/MessageType.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+/**
+ * Class MessageType
+ *
+ * @package AppBundle\Form
+ */
+class MessageType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array                $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username')
+            ->add('email')
+            ->add('content')
+        ;
+    }
+
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Message',
+        ));
+    }
+
+    /**
+     * Returns the name of this type.
+     *
+     * @return string The name of this type
+     */
+    public function getName()
+    {
+        return 'app_message';
+    }
+}
